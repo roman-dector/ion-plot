f90050fa22bff004751f6faab21e93d0515d84ab
roman-dector <roman.schaposhnikov@gmail.com>
2023-04-18 00:52:08 +0300

    asldf

--- plot/graph.py
+++ plot/graph.py
@@ -37,7 +37,7 @@ def plot_graph(
             reg = make_linear_regression(y_ax, x_ax, const)
             linspace = np.linspace(0, max(x_ax), 100)
 
-        ax.plot(linspace, reg.predict(sm.add_constant(linspace)), c=edgecolor)
+        ax.plot(reg.predict(sm.add_constant(linspace)), linspace, c=edgecolor)
         ax.set_title(
             f"{title}, k={round(reg.params[1], 3)}, k_err={round(reg.bse[1], 3)},\n\
             const={round(reg.params[0], 3)}, const_err={round(reg.bse[0], 3)}",
