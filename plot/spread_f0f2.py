38cba58227b8844058d76ca01ba353cfd71086e7
roman-dector <roman.schaposhnikov@gmail.com>
2023-04-25 04:22:54 +0300

    saf

--- plot/spread_f0f2.py
+++ plot/spread_f0f2.py
@@ -2,11 +2,8 @@ from pprint import pprint
 from plot.graph import plot_graph
 from plot.utils import (
     cast_data_to_dataframe,
-    get_month_days_count,
     make_linear_regression,
     split_df_to_sun_moon,
-    north_summer,
-    north_winter,
 )
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -17,6 +14,8 @@ from dal.models import (
 )
 from dal.handlers import (
     get_f0f2_k_spread_for_month,
+    get_f0f2_k_spread_for_summer_winter,
+    get_f0f2_k_spread_for_year,
 )
 
 
@@ -60,131 +59,12 @@ def calc_f0f2_k_mean_for_day(
     }
 
 
-def get_f0f2_k_spread_for_summer_winter(
-    ursi: str,
-    year: int,
-) -> list[float]:
-    sum_ion_sun = []
-    sum_ion_moon = []
-    sum_sat_sun = []
-    sum_sat_moon = []
-    sum_ion_sun_err = []
-    sum_ion_moon_err = []
-    sum_sat_sun_err = []
-    sum_sat_moon_err = []
-
-    win_ion_sun = []
-    win_ion_moon = []
-    win_sat_sun = []
-    win_sat_moon = []
-    win_ion_sun_err = []
-    win_ion_moon_err = []
-    win_sat_sun_err = []
-    win_sat_moon_err = []
-
-    if select_coords_by_ursi(ursi)['lat'] > 0:
-        sum_month = north_summer
-        win_month = north_winter
-    else:
-        sum_month = north_winter
-        win_month = north_summer
-
-    for m in sum_month:
-        try:
-            k_mean = calc_f0f2_k_mean_for_month(ursi, m, year)
-
-            sum_ion_sun.append(k_mean['ion']['sun']['k'])
-            sum_ion_moon.append(k_mean['ion']['moon']['k'])
-            sum_sat_sun.append(k_mean['sat']['sun']['k'])
-            sum_sat_moon.append(k_mean['sat']['moon']['k'])
-            sum_ion_sun_err.append(k_mean['ion']['sun']['err'])
-            sum_ion_moon_err.append(k_mean['ion']['moon']['err'])
-            sum_sat_sun_err.append(k_mean['sat']['sun']['err'])
-            sum_sat_moon_err.append(k_mean['sat']['moon']['err'])
-        except:
-            continue
-
-    for m in win_month:
-        try:
-            k_mean = calc_f0f2_k_mean_for_month(ursi, m, year)
-
-            win_ion_sun.append(k_mean['ion']['sun']['k'])
-            win_ion_moon.append(k_mean['ion']['moon']['k'])
-            win_sat_sun.append(k_mean['sat']['sun']['k'])
-            win_sat_moon.append(k_mean['sat']['moon']['k'])
-            win_ion_sun_err.append(k_mean['ion']['sun']['err'])
-            win_ion_moon_err.append(k_mean['ion']['moon']['err'])
-            win_sat_sun_err.append(k_mean['sat']['sun']['err'])
-            win_sat_moon_err.append(k_mean['sat']['moon']['err'])
-        except:
-            continue
-
-    return {
-        'ion': {
-            'sum': {
-                'sun': {'k': sum_ion_sun, 'err': sum_ion_sun_err},
-                'moon': {'k':  sum_ion_moon, 'err': sum_ion_moon_err},
-            },
-            'win': {
-                'sun': {'k': win_ion_sun, 'err': win_ion_sun_err},
-                'moon': {'k':  win_ion_moon, 'err': win_ion_moon_err},
-            },
-        },
-        'sat': {
-            'sum': {
-                'sun': {'k': sum_sat_sun, 'err': sum_sat_sun_err},
-                'moon': {'k':  sum_sat_moon, 'err': sum_sat_moon_err},
-            },
-            'win': {
-                'sun': {'k': win_sat_sun, 'err': win_sat_sun_err},
-                'moon': {'k':  win_sat_moon, 'err': win_sat_moon_err},
-            },
-        },
-    }
-
-
-def get_f0f2_k_spread_for_year(ursi: str, year: int):
-    ion_sun = []
-    ion_moon = []
-    ion_sun_err = []
-    ion_moon_err = []
-
-    sat_sun = []
-    sat_moon = []
-    sat_sun_err = []
-    sat_moon_err = []
-
-    for m in range(1, 13):
-        try:
-            k_mean = calc_f0f2_k_spread_for_month(ursi, m, year)
-
-            ion_sun.append(k_mean['ion']['sun']['k'])
-            ion_moon.append(k_mean['ion']['moon']['k'])
-            sat_sun.append(k_mean['sat']['sun']['k'])
-            sat_moon.append(k_mean['sat']['moon']['k'])
-            ion_sun_err.append(k_mean['ion']['sun']['err'])
-            ion_moon_err.append(k_mean['ion']['moon']['err'])
-            sat_sun_err.append(k_mean['sat']['sun']['err'])
-            sat_moon_err.append(k_mean['sat']['moon']['err'])
-        except:
-            continue
-
-    return {
-        'ion': {
-            'sun': {'k': ion_sun, 'err': ion_sun_err},
-            'moon': {'k':  ion_moon, 'err': ion_moon_err},
-        },
-        'sat': {
-            'sun': {'k': sat_sun, 'err': sat_sun_err},
-            'moon': {'k': sat_moon, 'err': sat_moon_err},
-        },
-    }
-
-
 def plot_f0f2_k_spread_for_month(
     ursi: str,
     month: int,
     year: int,
+    x_lim=(None, 10),
+    y_lim=(None, 20),
 ):
     coords = select_coords_by_ursi(ursi)
     ion_sun_k, ion_moon_k, sat_sun_k, sat_moon_k = get_f0f2_k_spread_for_month(ursi, month, year)
@@ -205,14 +85,14 @@ def plot_f0f2_k_spread_for_month(
     ax[1][0].grid()
     ax[1][1].grid()
     
-    ax[0][0].set_xlim(None, 10)
-    ax[0][0].set_ylim(None, 30)
-    ax[0][1].set_xlim(None, 10)
-    ax[0][1].set_ylim(None, 30)
-    ax[1][0].set_xlim(None, 10)
-    ax[1][0].set_ylim(None, 30)
-    ax[1][1].set_xlim(None, 10)
-    ax[1][1].set_ylim(None, 30)
+    ax[0][0].set_xlim(x_lim[0], x_lim[1])
+    ax[0][0].set_ylim(y_lim[0], y_lim[1])
+    ax[0][1].set_xlim(x_lim[0], x_lim[1])
+    ax[0][1].set_ylim(y_lim[0], y_lim[1])
+    ax[1][0].set_xlim(x_lim[0], x_lim[1])
+    ax[1][0].set_ylim(y_lim[0], y_lim[1])
+    ax[1][1].set_xlim(x_lim[0], x_lim[1])
+    ax[1][1].set_ylim(y_lim[0], y_lim[1])
 
     sns.histplot(ion_sun_k, kde=True, ax=ax[0][0])
     sns.histplot(ion_moon_k, kde=True, ax=ax[0][1])
@@ -221,22 +101,22 @@ def plot_f0f2_k_spread_for_month(
     
     mu_sum_sun, std_sum_sun = norm.fit(ion_sun_k)
     textstr_ion_sun = '\n'.join((
-    r'$\mu=%.2f$' % (mu_sum_sun, ),
+    r'$k=%.2f$' % (mu_sum_sun, ),
     r'$\sigma^2=%.2f$' % (std_sum_sun, )))
     
     mu_sum_moon, std_sum_moon = norm.fit(ion_moon_k)
     textstr_ion_moon = '\n'.join((
-    r'$\mu=%.2f$' % (mu_sum_moon, ),
+    r'$k=%.2f$' % (mu_sum_moon, ),
     r'$\sigma^2=%.2f$' % (std_sum_moon, )))
     
     mu_win_sun, std_win_sun = norm.fit(sat_sun_k)
     textstr_sat_sun = '\n'.join((
-    r'$\mu=%.2f$' % (mu_win_sun, ),
+    r'$k=%.2f$' % (mu_win_sun, ),
     r'$\sigma^2=%.2f$' % (std_win_sun, )))
     
     mu_win_moon, std_win_moon = norm.fit(sat_moon_k)
     textstr_sat_moon = '\n'.join((
-    r'$\mu=%.2f$' % (mu_win_moon, ),
+    r'$k=%.2f$' % (mu_win_moon, ),
     r'$\sigma^2=%.2f$' % (std_win_moon, )))
 
     # these are matplotlib.patch.Patch properties
@@ -297,22 +177,22 @@ def plot_f0f2_k_spread_for_summer_winter(
     
     mu_sum_sun, std_sum_sun = norm.fit(sum_sun_result)
     textstr_sum_sun = '\n'.join((
-    r'$\mu=%.2f$' % (mu_sum_sun, ),
+    r'$k=%.2f$' % (mu_sum_sun, ),
     r'$\sigma^2=%.2f$' % (std_sum_sun, )))
     
     mu_sum_moon, std_sum_moon = norm.fit(sum_moon_result)
     textstr_sum_moon = '\n'.join((
-    r'$\mu=%.2f$' % (mu_sum_moon, ),
+    r'$k=%.2f$' % (mu_sum_moon, ),
     r'$\sigma^2=%.2f$' % (std_sum_moon, )))
     
     mu_win_sun, std_win_sun = norm.fit(win_sun_result)
     textstr_win_sun = '\n'.join((
-    r'$\mu=%.2f$' % (mu_win_sun, ),
+    r'$k=%.2f$' % (mu_win_sun, ),
     r'$\sigma^2=%.2f$' % (std_win_sun, )))
     
     mu_win_moon, std_win_moon = norm.fit(win_moon_result)
     textstr_win_moon = '\n'.join((
-    r'$\mu=%.2f$' % (mu_win_moon, ),
+    r'$k=%.2f$' % (mu_win_moon, ),
     r'$\sigma^2=%.2f$' % (std_win_moon, )))
 
 
@@ -357,12 +237,12 @@ def plot_f0f2_k_spreading_for_year(ursi: str, year: int=2019):
     
     mu_sun, std_sun = norm.fit(sun_range)
     textstr_sun = '\n'.join((
-    r'$\mu=%.2f$' % (mu_sun, ),
+    r'$k=%.2f$' % (mu_sun, ),
     r'$\sigma^2=%.2f$' % (std_sun, )))
     
     mu_moon, std_moon = norm.fit(moon_range)
     textstr_moon = '\n'.join((
-    r'$\mu=%.2f$' % (mu_moon, ),
+    r'$k=%.2f$' % (mu_moon, ),
     r'$\sigma^2=%.2f$' % (std_moon, )))
 
     # these are matplotlib.patch.Patch properties
